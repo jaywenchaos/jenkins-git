67c313a0ebba8d901c2748375ff527147dfe4223
jaywenchaos <46684700+jaywenchaos@users.noreply.github.com>
2019-01-25 18:37:21 +0800

    Update test_jenkins.py

--- test_jenkins.py
+++ test_jenkins.py
@@ -1,9 +1,12 @@
+import allure
 class TestJenkins:
-
+    
+    @allure.step(title="test01执行步骤")
     def test01(self):
         print("test01")
         assert True
 
+    @allure.step(title="test02执行步骤")
     def test02(self):
         print("test02")
         assert False
